e1a2ccdbc6fde92fb5f00187f38f09a383bcccaf
kotaroTOP <135441002+kotaroTOP@users.noreply.github.com>
2024-11-26 20:27:05 +0300

    Create universal_Freaky_Go\Jo.lua

---
+++ universal_Freaky_Go\Jo.lua
@@ -0,0 +1,25 @@
+local uis = game:GetService("UserInputService")
+local rs = game.ReplicatedStorage
+local script_rs_folder = Instance.new("Folder", rs)
+script_rs_folder.Name = "Go/Jo_Freak_Script"
+local redF = Instance.new("Folder", script_rs_folder)
+local blueF = Instance.new("Folder", script_rs_folder)
+local char = game.Players.LocalPlayer.Character
+local hum = char.Humanoid
+local humanoidRP = char.HumanoidRootPart
+local redM = Instance.new("Part", redF)
+local redE = Instance.new("Particle", redF)
+local blueM = Instance.new("Part", blueF)
+local blueE = Instance.new("Particle", blueF)
+hum.DisplayName = "Go/Jo"
+redM.Name = "RedModel"
+redE.Name = "RedVFX"
+
+
+
+uis.InputBegan:Connect(function(Key, gpe) 
+  if gpe then end
+  if Key.KeyCode == Enum.KeyCode.E then 
+      
+  end
+end)
