7a7be7d900fd321b1a38bca13eb30387b3468187
kotaroTOP <135441002+kotaroTOP@users.noreply.github.com>
2024-11-27 09:58:28 +0300

    Update universal_Freaky_Go\Jo.lua

--- universal_Freaky_Go\Jo.lua
+++ universal_Freaky_Go\Jo.lua
@@ -1,11 +1,14 @@
 local uis = game:GetService("UserInputService")
 local rs = game.ReplicatedStorage
 local script_rs_folder = Instance.new("Folder", rs)
+local char = game.Players.LocalPlayer.Character
+local hum = char.Humanoid
+hum.MaxHealt = 1000
+hum.Health = hum.MaxHealth
 script_rs_folder.Name = "Go/Jo_Freak_Script"
 local redF = Instance.new("Folder", script_rs_folder)
 local blueF = Instance.new("Folder", script_rs_folder)
-local char = game.Players.LocalPlayer.Character
-local hum = char.Humanoid
+
 local humanoidRP = char.HumanoidRootPart
 local redM = Instance.new("Part", redF)
 local redE = Instance.new("Particle", redF)
