e4791fa98375bfdff3bee25e5c3b75d46f4cb815
kotaroTOP <135441002+kotaroTOP@users.noreply.github.com>
2024-11-27 14:46:56 +0300

    Update universal_Go\Kuna.lua

--- universal_Go\Kuna.lua
+++ universal_Go\Kuna.lua
@@ -1,4 +1,4 @@
-wait(10)
+task.wait(10)
 local uis = game:GetService("UserInputService")
 local rs = game.ReplicatedStorage
 local script_rs_folder = Instance.new("Folder", rs)
@@ -20,31 +20,23 @@ redM.BrickColor = BrickColor.new("Persimmon")
 redM.Size = Vector3.new(1, 1, 1)
 uis.InputBegan:Connect(function(Key, gpe) 
 	if gpe then end
-	if Key.KeyCode == Enum.KeyCode.P then 
-		hum.Health += 30
+	if Key.KeyCode == Enum.KeyCode.P then
+		for i=0, hum.MaxHealth do
+			hum.Health += i
+			task.wait(0.01)
+		end
 	end
 	if Key.KeyCode == Enum.KeyCode.E then
 		local redC = redM:Clone()
 		for i=0, 300 do
-			redC.CFrame = redC.CFrame + Vector3.new(0.1, 0.1, 0.1)
+			redC.Position = humRP.Position
+			redC.Position = redC.Position + Vector3.new(0.1, 0.1, 0.1)
 			if i == 300 then
 				redC.Size = redC.Size + Vector3.new(10, 10, 10)
 				wait(1)
 				redC:Destroy()
 			end
 		end
-	end
-	if Key.KeyCode == Enum.KeyCode.T then
-		local found
-		local last = math.huge
-		for _,plyr in pairs(game.Players:GetPlayers()) do
-			local distance = plyr:DistanceFromCharacter(char.CFrame)
-			if distance < last then
-				found = plyr
-				last = distance
-			end
-		end
-		humRP:MoveTo(found)
 	end
 end)
 hum.StateChanged:Connect(function(_oldState, newState)
@@ -54,3 +46,18 @@ hum.StateChanged:Connect(function(_oldState, newState)
 		hum:ChangeState(Enum.HumanoidStateType.None)
 	end
 end)
+while true do
+	local currentHealth = hum.Health
+	if hum.Health ~= currentHealth
+		local found
+		local last = math.huge
+		for _,plyr in pairs(game.Players:GetPlayers()) do
+			local distance = plyr:DistanceFromCharacter(char.CFrame)
+			if distance < last then
+				found = plyr
+				last = distance
+			end
+		end
+		humRP.Position = found.HumanoidRootPart.Position + Vector3.new(1,1,1)
+		humRP.Rotation = humRP.Rotation + Vector3.new(0, 180, 0)
+	end
